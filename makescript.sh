c6e5fe8ec6bf1bb7939247e48233af23a28fdf72
root <root@linux200.example.com>
2024-09-27 20:25:56 +0900

    first commit

---
+++ makescript.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Variable definition
+ROOT=/root/scripts
+FILENAME=$1
+
+# Function definition
+MakeNewFile() {
+    cat << EOF > $ROOT/$FILENAME
+#!/bin/bash
+
+
+EOF
+
+    chmod 700 $ROOT/$FILENAME
+    echo "[ NEW ] $FILENAME이 생성되었습니다."
+    exit
+}
+
+Exit() {
+    echo "파일 생성을 중지합니다."
+    exit
+}
+
+# Main function
+if [ -f $FILENAME ] ; then
+    while true
+    do
+        echo -n "[ WARN ] 파일이 존재합니다. 새로 생성하겠습니까? (y/n): "
+        read ANSWER
+        case $ANSWER in
+            y|Y|yes|YES) MakeNewFile ;;
+            n|N|no|NO)   Exit ;;
+            *) continue ;;
+        esac
+    done
+else
+    MakeNewFile
+fi
