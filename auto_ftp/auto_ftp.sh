72ccbdecefdb409703028c51fb9427d4b6b046e6
9rrrr-m <9rrrrm@google.com>
2024-09-29 12:01:03 +0900

    [Update] auto_ftp.sh

--- auto_ftp/auto_ftp.sh
+++ auto_ftp/auto_ftp.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-ftp -n 127.0.0.1 21 << EOF
-user testuser password
-cd /tmp
-lcd /root/scripts/test
-bin
-hash
-prompt
-mput testfile.txt
-quit
+# Variable Definition
+SERVER_LIST=/root/scripts/test/serverlist.txt
+
+# Main Function
+# FTP 수행
+for IP in `cat $SERVER_LIST`
+do
+    ftp -n $IP << EOF
+    user testuser password
+    cd /tmp
+    lcd /root/scripts/test
+    bin
+    hash
+    prompt
+    mput testfile.txt
+    quit
 EOF
+done
