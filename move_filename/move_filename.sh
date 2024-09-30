698073f2eff6c0a0bfd60ddbc6bb05a586592e57
9rrrr-m <9rrrrm@google.com>
2024-09-30 19:37:37 +0900

    [Update] move_filename.sh

--- move_filename/move_filename.sh
+++ move_filename/move_filename.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+# Usage
+if [ $# -ne 3 ] ; then
+    echo "Usage: $0 <directory> <orig_ext> <new_ext>"
+    exit 1
+fi
+
 # Variable Definition
-D_WORK=/root/scripts/move_filename    # working directory
-T_FILE=/tmp/.tmp1                     # temporary file
+D_WORK=$1            # working directory
+ORIG=$2              # original extension
+NEW=$3               # new extension
+T_FILE=/tmp/.tmp1    # temporary file
 
 # Main Function
-ls -1 $D_WORK | grep '.txt$' > $T_FILE
+ls -1 $D_WORK | grep ".$ORIG$" > $T_FILE
 for FILE in $(cat $T_FILE)
 do
-    mv $D_WORK/$FILE $(echo $D_WORK/$FILE | sed 's/.txt$/.els/g')
+    mv $D_WORK/$FILE $(echo $D_WORK/$FILE | sed "s/.$ORIG$/.$NEW/g")
 done
