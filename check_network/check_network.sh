9fe6f32265d9c3f082bac30c2fc52b715778b7e5
9rrrr-m <9rrrrm@google.com>
2024-10-31 19:35:53 +0900

    [Update] check_network.sh

--- check_network/check_network.sh
+++ check_network/check_network.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # Variable Definition
-IP1=192.168.10.2      # local gateway ip
+# local gateway ip
+echo -n "Gateway IP address(default: 192.168.10.2)? : "
+read IP1
+IP1="${IP1:-192.168.10.2}"
+
 IP2=168.126.63.1      # external ip
 IP3=www.google.com    # domain address
 
